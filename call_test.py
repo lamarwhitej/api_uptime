9c19d7ada37846becbcc2dd1fe25744d4f73fd22
lamarwhitej <lamarwhitej@yahoo.com>
2016-08-26 17:51:11 -0500

    Update call_test.py

--- call_test.py
+++ call_test.py
@@ -9,13 +9,13 @@ import test
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self):
         desc = "Tests the uptime for a given length of time against a list of services."
-        usage_string = "api-uptime [-i/--server-id] [-s/--services] [-t/--time]"
+        usage_string = "api-uptime [-i/--subnet-id] [-s/--services] [-t/--time] [-c/--container-name]"
 
         super (ArgumentParser, self).__init__(
             usage=usage_string, description=desc)
 
         self.add_argument(
-            "-i", "--server-id", metavar="<server id>",
+            "-i", "--subnet-id", metavar="<subnet id>",
             required=False, default=None)
 
         self.add_argument(
@@ -25,6 +25,10 @@ class ArgumentParser(argparse.ArgumentParser):
         self.add_argument(
             "-t", "--times", metavar="<amount of seconds to run>",
             required=False, default=60, type=int)
+            
+        self.add_argument(
+            "-c", "--container-name", metavar="<name of swift container>",
+            required=False, default=None)
 
 
 def entry_point():
@@ -39,7 +43,8 @@ def entry_point():
     tenant = config.get("openstack", "tenant")
     auth_url = config.get("openstack", "auth_url")
     services_list = config.get("openstack", "services_list")
-    server_id = cl_args.server_id or config.get("openstack", "server_id")
+    subnet_id = cl_args.subnet_id or config.get("openstack", "subnet_id")
+    container_name = cl_args.container_name or config.get("openstack", "container_name")
 
     services = [service.strip() for service in (cl_args.services or services_list).split(",")]
 
@@ -49,7 +54,7 @@ def entry_point():
     for s in services:
         p, c = Pipe()
         pipes.append(p)
-        Process(target=mad.uptime, args=(c,s,cl_args.times,server_id,)).start()
+        Process(target=mad.uptime, args=(c,s,cl_args.times,subnet_id,container_name,)).start()
         c.close()
 
     outputs = [pipe.recv() for pipe in pipes]
