f0dcaaef4fa490efa74e32ca9a7c5d43d56b8794
lamarwhitej <lamarwhitej@yahoo.com>
2016-09-13 13:49:14 -0500

    Update call_test.py

--- call_test.py
+++ call_test.py
@@ -2,6 +2,8 @@ import argparse
 from ConfigParser import SafeConfigParser
 import json
 from multiprocessing import Pipe, Process
+import os
+import sys
 
 import test
 
@@ -9,7 +11,7 @@ import test
 class ArgumentParser(argparse.ArgumentParser):
     def __init__(self):
         desc = "Tests the uptime for a given length of time against a list of services."
-        usage_string = "api-uptime [-s/--services] [-t/--time]"
+        usage_string = "[-s/--services] ([-t/--time] | [-d/--daemon]) [-o/--output-file]"
 
         super (ArgumentParser, self).__init__(
             usage=usage_string, description=desc)
@@ -18,9 +20,18 @@ class ArgumentParser(argparse.ArgumentParser):
             "-s", "--services", metavar="<comma-delimited services list>",
             required=False, default=None)
 
-        self.add_argument(
+        group = self.add_mutually_exclusive_group()
+        group.add_argument(
             "-t", "--times", metavar="<amount of seconds to run>",
             required=False, default=60, type=int)
+        group.add_argument(
+            "-d", "--daemon", required=False, action='store_true')
+
+        self.add_argument(
+            "-o", "--output-file", metavar="<path to output file>",
+            required=False, default=None)
+
+
 
 def entry_point():
     cl_args = ArgumentParser().parse_args()
@@ -34,22 +45,40 @@ def entry_point():
     tenant = config.get("openstack", "tenant")
     auth_url = config.get("openstack", "auth_url")
     services_list = config.get("openstack", "services_list")
+    daemon_file = config.get("openstack", "daemon_file") or os.path.join(sys.prefix, "api.uptime.stop")
+    output_file = cl_args.output_file or config.get("openstack", "output_file")
+
+    if cl_args.daemon and os.path.exists(daemon_file):
+        os.remove(daemon_file)
 
     services = [service.strip() for service in (cl_args.services or services_list).split(",")]
 
     mad = test.ApiUptime(version, user, password, tenant, auth_url)
 
+    time_value = cl_args.daemon if cl_args.daemon else cl_args.times
+
     pipes = []
     for s in services:
         p, c = Pipe()
         pipes.append(p)
-        Process(target=mad.uptime, args=(c,s,cl_args.times,)).start()
+        Process(target=mad.uptime, args=(c,s,time_value,)).start()
         c.close()
 
-    outputs = [pipe.recv() for pipe in pipes]
+    if cl_args.daemon:
+        while True:
+            if os.path.exists(daemon_file):
+                for pipe in pipes:
+                    pipe.send("STOP")
+                break
+
+	outputs = [pipe.recv() for pipe in pipes]
     final_output = {k: v for d in outputs for k, v in d.items()}
 
-    print json.dumps(final_output)
+    if output_file is None or output_file == '':
+        print json.dumps(final_output)
+    else:
+        with open(output_file, 'w') as out:
+            out.write(json.dumps(final_output))
 
 
 if __name__ == "__main__":
